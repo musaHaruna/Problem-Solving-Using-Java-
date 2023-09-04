c9b83c6ee10c6c0508af5d1dc450a0a78acd4ff1
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-09-04 09:25:29 -0700

    add problem statement (day of the week)

--- Elements of Programming/1.2 Built-in data types/Creative Exercise/DayOfTheWeek.java
+++ Elements of Programming/1.2 Built-in data types/Creative Exercise/DayOfTheWeek.java
@@ -1,3 +1,14 @@
+// Day of the week. Write a program that takes a date as input and prints the
+// day of the week that date falls on. Your program should accept three int commandline
+// arguments: m (month), d (day), and y (year). For m, use 1 for January, 2 for
+// February, and so forth. For output, print 0 for Sunday, 1 for Monday, 2 for Tuesday,
+// and so forth. Use the following formulas, for the Gregorian calendar:
+// y0 = y  (14  m) / 12
+// x = y0  y0 / 4  y0 / 100  y0 / 400
+// m0 = m  12  ((14  m) / 12)  2
+// d0 = (d  x  (31  m0) / 12) % 7
+
+
 public class DayOfTheWeek {
     public static void main(String[] args) {
         // Define the date components (m, d, y)
