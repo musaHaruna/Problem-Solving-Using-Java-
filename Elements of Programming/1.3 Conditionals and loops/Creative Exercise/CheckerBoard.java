4c40678ea10643d613ec17f80d1bae4818f60f11
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-09-06 14:25:36 -0700

    checker board problem

---
+++ Elements of Programming/1.3 Conditionals and loops/Creative Exercise/CheckerBoard.java
@@ -0,0 +1,15 @@
+public class CheckerBoard {
+ public static void main(String[] args) {
+  int n = 8;
+for (int i = 1; i <= n; i++) { // Print the ith line.
+    for (int j = 1; j <= n; j++) { // Print the jth element in the ith line.
+        if (((i + j) % 2 == 0 )) {
+            System.out.print("*");
+        }else
+        System.out.print(" ");
+     }
+System.out.println(i);
+}
+}
+}
+
