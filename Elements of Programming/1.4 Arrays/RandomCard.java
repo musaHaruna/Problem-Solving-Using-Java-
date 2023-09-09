93e9ddd80fd93da4ddc4b027176b7d37f16a4892
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-09-09 10:14:55 -0700

    update random card indentation

--- Elements of Programming/1.4 Arrays/RandomCard.java
+++ Elements of Programming/1.4 Arrays/RandomCard.java
@@ -1,9 +1,9 @@
 public class RandomCard {
- public static void main(String[] args) {
-   String[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades" };
-   String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-   int i = (int) (Math.random() * RANKS.length);
-   int j = (int) (Math.random() * SUITS.length);
-  System.out.println(RANKS[i] + " of " + SUITS[j]);
- }
+    public static void main(String[] args) {
+        String[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades" };
+        String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        int i = (int) (Math.random() * RANKS.length);
+        int j = (int) (Math.random() * SUITS.length);
+        System.out.println(RANKS[i] + " of " + SUITS[j]);
+    }
 }
